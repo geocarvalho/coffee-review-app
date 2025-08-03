aa9003ed5de5bed8535d64ae34509af4824dc79e
George Carvalho <geocarvalho91@gmail.com>
2025-08-03 00:36:28 -0700

    Add cache-busting headers to force CSS reload

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,17 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 } 
